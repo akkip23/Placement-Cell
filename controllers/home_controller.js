52c91e368581ac804d0579e433e2b86e082d915e
Akshay <akshaypawle23@gmail.com>
2023-08-06 12:33:14 +0400

    comments updated

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,6 +4,7 @@ const Student = require("../model/students");
 module.exports.home = async function (req, res) {
   try {
     const studentData = await Student.find({});
+
     // console.log("studentData", studentData);
     res.render("home", {
       title: "Placement Cell",
